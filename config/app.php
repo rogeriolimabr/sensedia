aaebe47a3e124d31d5df97c055d77d64fc5936c6
Rogerio Lima <rrocha@alpekpolyester.com>
2021-08-15 22:44:18 -0300

    Informando o HOST no arquivo de configuração

--- config/app.php
+++ config/app.php
@@ -5,5 +5,5 @@ define('NAME', 'Sensedia');
 
 // URLs
 define('APP_ROOT', dirname(dirname(__FILE__)));
-define('URL_ROOT', '/');
+define('URL_ROOT', 'http://3.139.11.151/');
 define('URL_SUBFOLDER', '');
